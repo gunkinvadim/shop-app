a43de5eebae8bae43fb5bb890c1138ac834af42b
gunkinvadim <46325759+gunkinvadim@users.noreply.github.com>
2026-03-25 14:21:14 +0500

    static assets

--- shop-nestjs-backend/src/main.ts
+++ shop-nestjs-backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
   console.log("bootstrap")
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(process.cwd(), 'uploads'), { prefix: '/uploads' });
   app.enableCors({
     origin: 'http://localhost:3001',
     credentials: true,
